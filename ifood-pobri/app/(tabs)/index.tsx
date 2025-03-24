8ae2d359200d27611f9a662e2efcff665122a197
caio.silveira <caiosgs.pss@gmail.com>
2025-03-24 17:37:46 -0300

    Em edição(1)

--- ifood-pobri/app/(tabs)/index.tsx
+++ ifood-pobri/app/(tabs)/index.tsx
@@ -2,13 +2,14 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { useState } from 'react';
 
-export default function TabOneScreen() {
+
+export default function TabOneScreen(){
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
+      <Text style={styles.title}>Cardápio Bar do Tião</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
